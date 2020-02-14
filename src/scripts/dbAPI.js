c7ce6304b312334f4195d9c26a1fda8d4fa8600b
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-02-14 13:16:52 -0600

    added CRUD fetch calls to dbAPI.js

--- src/scripts/dbAPI.js
+++ src/scripts/dbAPI.js
@@ -5,5 +5,41 @@ const dbAPI = {
     getObjectByResource(resource, userId) {
         return fetch(`${baseUrl}/${resource}/?userId=${userId}&_expand=user`)
                 .then(resp => resp.json())
+    },
+    postObjectByResource(resourceObject,resource) {
+        return fetch((`${baseUrl}/${resource}`), {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(resourceObject)
+        })
+            .then(resp => resp.json())
+    },
+    deleteObjectByResource(resource, id) {
+        return fetch(`${baseUrl}/${resource}/${id}`, {
+            method: "DELETE"
+        })
+    },
+    putObejectByResource(resourceObject, resource) {
+        return fetch(`${baseUrl}/${resource}/${resourceObject.id}`,{
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(resourceObject)
+        })
+            .then(resp => resp.json())
+    },
+    patchObejectByResource(resourceObject, resource) {
+        return fetch(`${baseUrl}/${resource}/${resourceObject.id}`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(resourceObject)
+        })
+            .then(resp => resp.json())
     }
 }
+
