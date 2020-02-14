9d1d304f57e4a30d8f08f94726a01e3f34ff4849
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-02-14 11:49:07 -0600

    fixed error in fetch url

--- src/scripts/dbAPI.js
+++ src/scripts/dbAPI.js
@@ -3,7 +3,7 @@ baseUrl = "http://localhost:8088"
 
 const dbAPI = {
     getObjectByResource(resource, userId) {
-        return fetch(`${baseUrl}/${resource}?id=${userId}&_expand=user`)
+        return fetch(`${baseUrl}/${resource}/?userId=${userId}&_expand=user`)
                 .then(resp => resp.json())
     }
 }
